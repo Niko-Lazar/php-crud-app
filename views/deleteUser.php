16763a9eb3e9acecee3b4c75412b12ebbcb211bc
Lazar Nikolic <nikoliclazarln@gmail.com>
2022-10-08 19:26:33 +0200

    Fixed delete user route problem

--- views/deleteUser.php
+++ views/deleteUser.php
@@ -1,8 +1,10 @@
 <?php require '../config/database.php' ?>
+<?php require 'globals.php'; ?>
 <?php
+session_start();
 
 if(!loggedIn()) {
-    header('Location: login.php');
+    header('Location: ../templates/login.php');
     exit();
 }
 
@@ -14,11 +16,12 @@ $userID = $_REQUEST['id'];
     
 $sql = "DELETE FROM users WHERE id=${userID}";
 
-$queryIsSuccessful = $conn->query($sql) === TRUE;
+$queryIsSuccessful = $conn->query($sql);
 
 if(!$queryIsSuccessful) {
     echo "Error deleting user " . $conn->error;
 }
+
 header('Location: ../templates/users.php?user-action=2');
 exit();
 
